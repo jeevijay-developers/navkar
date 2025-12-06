60d40016b5e2d31b277a8ab93751a15e9fc5d7dc
icodelakshay <lakshaysaxena2217@gmail.com>
2025-12-06 17:19:48 +0530

    Refactor FaqContact component: update contact data structure and improve icon handling

--- src/components/inner-pages/faq/FaqContact.tsx
+++ src/components/inner-pages/faq/FaqContact.tsx
@@ -2,6 +2,7 @@ interface DataType {
   id: number;
   data_wow_delay: string;
   title: string;
+  icon: string;
   contact_1: JSX.Element;
   contact_2: JSX.Element;
 }
@@ -10,37 +11,25 @@ interface DataType {
 const faq_contact_data: DataType[] = [
   {
     id: 1,
-    data_wow_delay: ".3s",
-    title: "Phone & Fax",
-    contact_1: (
-      <>
-        Mobile : <a href="tel:+912646251639">+91 2646 251639</a>
-      </>
-    ),
-    contact_2: (
-      <>
-        Mobile : <a href="tel:+912646221638">+91 2646 221638</a>
-      </>
-    ),
-  },
-  {
-    id: 2,
     data_wow_delay: ".5s",
     title: "Mail Address",
+    icon: "fa-solid fa-envelope",
     contact_1: <>info@shrinavkarblowpack.com</>,
     contact_2: <>info@shrinavkarblowpack.com</>,
   },
   {
-    id: 3,
+    id: 2,
     data_wow_delay: ".7s",
     title: "Factory Locations",
+    icon: "fa-solid fa-location-dot",
     contact_1: <>Ankleshwar, Gujarat</>,
     contact_2: <>Panoli, Gujarat</>,
   },
   {
-    id: 4,
+    id: 3,
     data_wow_delay: ".9s",
     title: "Office Hour",
+    icon: "fa-solid fa-clock",
     contact_1: <>Mon to Sat: 9:00 AM to 6:00 PM</>,
     contact_2: <>Mon to Sat: 9:00 AM to 6:00 PM</>,
   },
@@ -59,12 +48,12 @@ const FaqContact = ({ padding }: any) => {
             {faq_contact_data.map((item) => (
               <div
                 key={item.id}
-                className="col-xl-3 col-lg-4 col-md-6 wow fadeInUp"
+                className="col-lg-4 col-md-6 wow fadeInUp"
                 data-wow-delay={item.data_wow_delay}
               >
                 <div className="contact-info-items">
                   <div className="icon">
-                    <i className="fa-solid fa-phone"></i>
+                    <i className={item.icon}></i>
                   </div>
                   <div className="content">
                     <h3>{item.title}</h3>
